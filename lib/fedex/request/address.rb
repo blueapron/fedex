3d2da637e45de5e32a3b99ff52ff69bc9c70510b
Evan Whalen <evanwhalen@gmail.com>
2015-01-14 16:52:28 -0500

    Add error code to rate error.

--- lib/fedex/request/address.rb
+++ lib/fedex/request/address.rb
@@ -22,12 +22,16 @@ module Fedex
           options = options.first if options.is_a? Array
           Fedex::Address.new(options)
         else
-          error_message = if response[:address_validation_reply]
-            [response[:address_validation_reply][:notifications]].flatten.first[:message]
+          if response[:address_validation_reply]
+            notifications = response[:address_validation_reply][:notifications]
+            notification = [notifications].flatten.first
+            error_message = notification[:message]
+            error_code = notification[:code]
           else
-            "#{api_response["Fault"]["detail"]["fault"]["reason"]}\n--#{api_response["Fault"]["detail"]["fault"]["details"]["ValidationFailureDetail"]["message"].join("\n--")}"
+            error_message = api_response["Fault"]["detail"]["fault"]["reason"]
+            error_code = api_response["Fault"]["detail"]["fault"]["errorCode"]
           end rescue $1
-          raise RateError, error_message
+          raise RateError.new(error_message, code: error_code)
         end
       end
 
